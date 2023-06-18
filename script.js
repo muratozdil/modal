be08b04803cbafbd991945688987a8929f698f27
muratozdil <murat.ozdil007@gmail.com>
2023-06-18 15:40:11 +0300

    final form for future notice

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+const btnOpen = document.querySelectorAll('.show-modal');
+//since theres more than more show-modal button 'all' works like an array
+
+//to open modal
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+//to close modal
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpen.length; i++)
+    btnOpen[i].addEventListener('click', openModal);
+
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+    if (
+        event.key === 'Escape' &&
+        !modal.classList.contains('hidden') &&
+        !overlay.classList.contains('hidden')
+    )
+        closeModal();
+});
